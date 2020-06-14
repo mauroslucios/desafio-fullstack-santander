eca7f1c3fd19ac530ff32e67a8e2816f1c09dbd1
mauroslucios <mauroslucios@gmail.com>
2020-06-14 16:51:59 -0300

    Desafio Primeira Etapa Pronto

--- desafios/js/exercicio02.js
+++ desafios/js/exercicio02.js
@@ -1,8 +1,26 @@
-function produto(multiplos){
-    var produto = 1;
-    for(var i = 1; i <= multiplos.length; i++){
-        produto *= multiplos[i];
-        return produto;  
-    }
-}
-console.log(produto([1, 2, 7]) );
+// function produto(multiplos){
+//     var produto = 1;
+//     for(const multiplo of multiplos){
+//         produto *= multiplo;
+//         return produto;  
+//     }
+// }
+// console.log(produto([1, 2, 7]) );
+/**
+ *
+ *
+ * @param {*} multiplos
+ * @returns
+ */
+function mult(multiplos) {
+
+    let totMult = 1
+   
+    for (const mult of multiplos) 
+    totMult *= mult
+   
+    return totMult
+   
+   }
+   
+   console.log(mult(1, 4, 7))
--- desafios/js/exercicio06.js
+++ desafios/js/exercicio06.js
@@ -1,12 +1,26 @@
-function alturaArvoreUtopica(ciclos){
-    var arvore = 1;
-    for(var i = 0;i < ciclos; i++){
-        if(i & 1){
-            arvore += 1;
-        }else{
-            arvore *= 2;
-        }
-        return arvore;
-    }
-}
+/**
+ *
+ *
+ * @param {*} ciclos
+ * @returns
+ */
+function alturaArvoreUtopica (ciclos) {
+    var altura = 1;
+    if (ciclos == 0) 
+      altura = 1;
+    else
+      for (var i=1; i<=ciclos; i++) {
+        if (i % 2 == 0)
+          altura += 1;
+        else
+          altura = altura*2;
+      }
+    return altura;
+  }
+console.log(alturaArvoreUtopica(0));
+
 console.log(alturaArvoreUtopica(1));
+
+console.log(alturaArvoreUtopica(2));
+
+console.log(alturaArvoreUtopica(3));
